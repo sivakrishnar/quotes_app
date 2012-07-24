6121f1fdb22611a0c2a5e56614c6def78826623c
Siva K Reddybathula <siva.reddybathula@gmail.com>
2012-07-23 18:34:21 -0700

    added bootstrap css files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,7 +25,8 @@ module ApplicationHelper
   end
   
   def getAppUrl()
-   return 'http://quotesapp.heroku.com/'
+   return 'http://localhost:3000/'
+   ##return 'http://quotesapp.heroku.com/'
   end
 
   def uri_escape(url)
