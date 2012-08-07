10a596041fc34381f8dc7d5cc719fcfc7296a544
Siva K Reddybathula <siva.reddybathula@gmail.com>
2012-08-07 00:56:53 -0700

    adding oauth for twitter authentication

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -12,13 +12,14 @@ class UserSessionController < ApplicationController
     client = TwitterOAuth::Client.new(
       :consumer_key => oauth_token,
       :consumer_secret => oauth_token_secret)
-    oauth_confirm_url = getAppUrl+'login/twitter/callback'  
+    oauth_confirm_url = getAppUrl+'login/twitter/callback'
+    puts oauth_confirm_url
     request_token = client.request_token(:oauth_callback => oauth_confirm_url)
     #:oauth_callback required for web apps, since oauth gem by default force PIN-based flow 
     #( see http://groups.google.com/group/twitter-development-talk/browse_thread/thread/472500cfe9e7cdb9/848f834227d3e64d )
    
   
-   request_token.authorize_url
+   request_token.authorize_url+"?oauth_token=#{request_token.Token}"
    access_token = client.authorize(
      request_token.token,
      request_token.secret,
