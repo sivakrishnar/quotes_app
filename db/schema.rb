c2038017f4eda2c3f4d105b9da4c00ece119a9eb
Siva K Reddybathula <siva.reddybathula@gmail.com>
2012-08-14 23:34:18 -0700

    removing users code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120810062619) do
+ActiveRecord::Schema.define(:version => 20120815062840) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -25,15 +25,6 @@ ActiveRecord::Schema.define(:version => 20120810062619) do
     t.string   "tags"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
